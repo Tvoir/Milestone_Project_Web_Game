a0329e5b8b7a96b69de1cd77931762fe72d6656f
Tvoir <TommyVoir@yahoo.com>
2022-08-21 11:20:03 -0700

    created a loop for the hoop to go back and forth... forever...

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 // if object is at this cooridinate and this cooridinate then count as a score 
-
+let bodywidth = document.querySelector("body").style.backgroundcolor = "green";
 const hoop = movingHoop(150, 350)
 
-// hoop.walkEast(4000, () => {
-//     npc.walkWest(4000)                                                   
-// })
 
-// newImage('assets/hoop.png', 500,350)
-// newImage('assets/ball.png', 550,10)
 
-// const hoop = movingHoop(600, 500)
+
+
 
 async function moveHoop(){
-    await hoop.walkEast(3000)
-    await hoop.walkWest(3000)
-    
+    while(true){
+        await hoop.walkEast(3000)
+            await hoop.walkWest(3000)
+            
+}
+
 }
 moveHoop()
-
-// document.querySelector("body").style.color = "Red"
