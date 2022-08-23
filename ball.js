7d38732647cce461f2cdae8ea67c75ca25f7a820
Tvoir <TommyVoir@yahoo.com>
2022-08-23 15:45:00 -0700

    found a collision code... but not working..

--- ball.js
+++ ball.js
@@ -1,6 +1,22 @@
-let ball = document.querySelector('.ball');
-let shootLength = 400;
+let shootLength = 450;
 let ballPosition = 0;
+let score = 0;
+
+// let dim1 = {x: 5, y: 5, w: 50, h: 50}
+// let dim2 = {x:20, y:10, w: 60, h: 40}
+
+// const bBall = attr(dim1);
+// const rim = attr(dim2);
+var ball = document.querySelector('.ball');
+var rim = document.getElementsByName('hoop');
+// class ball {
+//     constructor(){
+//         this.x = 10;
+//         this.y = 50;
+//         this.width = 100;
+//         this.height = 100;
+//     }
+// }
 
     window.addEventListener('load' , () => {
         ball.style.position = 'absolute';
@@ -14,7 +30,14 @@ let ballPosition = 0;
                 let throwInterval = setInterval(() => {
                 if( ballPosition <= shootLength
                     ) { //check for collision here (checks the top position of bball against bottom position of rim/ bottom position of bball to top pos of rim.)
-                        // if (ball.style.top >= rim.style.bottom || ball.style.top <= rim.style.top) 
+                        if (ball.x < rim.x + rim.w &&
+                            ball.x + ball.w > rim.x &&
+                            ball.y < rim.y + rim.h &&
+                            ball.h + ball.y > rim.y) {
+                                //collsion detected
+                                console.log("collision detected")
+                                score = 1
+                            }
                         // detect collision js in google
                     ball.style.bottom = `${ballPosition}px`
                 ballPosition += 10}
@@ -24,13 +47,37 @@ let ballPosition = 0;
                     if (ballPosition > 0) {ballPosition -= 10
                         ball.style.bottom = `${ballPosition}px`}
                         else {clearInterval(fallInterval)}
-                }, 40)}
-            }, 40);
+                }, 20)}
+            }, 20);
                 // ball.style.bottom = parseInt(ball.style.bottom) + shootLength + 'px';
                 break;
             }
         })
 
+       
+// function Circle(x, y, radius, color) {
+//     this.x = x;
+//     this.y = y;
+//     this.radius = radius;
+//     this.color = color;
+
+//     this.update = function() {
+//         this.draw();
+//     };
+    
+//     this.draw = function() {
+//         c.beginpath();
+//         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+//         c.fillStyle = this.color;
+//         c.fill();
+//         c.closePath();
+//     };
+// }
+//         let circle1 = document.querySelector('.circle');
+//         function init() {
+//             circle1 = new Circle (300, 300, 100, 'black');
+//         }
+//         let circle2 = document.querySelector('movingHoop');
         //attach a rim div to the basketball hoop using position absolute on css
         //every frame a basketball moves run a collision detection that calculates 
 
--- movingHoop.js
+++ movingHoop.js
@@ -8,14 +8,8 @@ function movingHoop(x, y) {
         if (direction === 'west') {
             x -= 1
         }
-        if (direction === 'north') {
-            y += 1
-        }
         if (direction === 'east') {
             x += 1
-        }
-        if (direction === 'south') {
-            y -= 1
         }
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
@@ -36,18 +30,11 @@ function movingHoop(x, y) {
         await sleep(time);
     }
 
-    function stop() {
-        direction = null
-        element.src = `./assets/hoop.png`
-    }
 
     return {
         element: element,
         walkWest: walkWest,
-        // walkNorth: walkNorth,
         walkEast: walkEast,
-        // walkSouth: walkSouth,
-        stop: stop
     }
 }
 
